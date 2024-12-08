f660f4c365d16fb0a0a67ed87689c595b2211a6d
Elango <85941190+codeofelango@users.noreply.github.com>
2024-12-08 16:28:30 +0300

    Update udf.js

--- udf.js
+++ udf.js
@@ -1,12 +1,18 @@
 function transform(line) {
-    var values = line.split(',');
-    var obj = new Object();
+  // Split the line into an array of values using the comma as a delimiter.
+  var values = line.split(',');
   
-    obj.id = values[0];
-    obj.name = values[1];
-    obj.age = values[2];
+  // Create an object to store the parsed data.
+  var obj = new Object();
 
+  // Assign the values from the array to the corresponding properties of the object.
+  obj.id = values[0];
+  obj.name = values[1];
+  obj.age = values[2];
+
+  // Convert the object into a JSON string.
+  var jsonString = JSON.stringify(obj);
   
-    var jsonString = JSON.stringify(obj);
-    return jsonString;
-  }
+  // Return the JSON string.
+  return jsonString;
+}
